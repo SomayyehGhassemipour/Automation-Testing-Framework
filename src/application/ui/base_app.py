e409825930f860e88236dabd261411e84f3c5902
somayyeh ghassemipour <97106176+SomayyehGhassemipour@users.noreply.github.com>
2024-05-21 10:24:27 +0200

    more detailed in BaseApp

--- src/application/ui/base_app.py
+++ src/application/ui/base_app.py
@@ -5,14 +5,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 class BaseApp:
-  def __init__(self, browser ) -> None:
+  def __init__(self, browser: webdriver ) -> None:
     self.browser = browser
 
   def navigate_to(self, url):
     self.browser.get(url)
 
   def enter_input(self, locator: str, text: str):
-    input = WebDriverWait(self.browser, 2).until(EC.element_to_be_clickable((By.XPATH, locator)))
+    # input = WebDriverWait(self.browser, 2).until(EC.element_to_be_clickable((By.XPATH, locator)))
+    input = self.browser.find_element(By.XPATH, locator)
     input.clear()
     input.send_keys(text)
 
@@ -20,10 +21,12 @@ class BaseApp:
     sign_in_button = WebDriverWait(self.browser, 2).until(
             EC.element_to_be_clickable((By.XPATH, locator))
         )
+    # sign_in_button = self.browser.find_element(By.XPATH, locator)
     sign_in_button.click()
   
   def get_text(self, locator: str):
     error_message_element = WebDriverWait(self.browser, 2).until(EC.visibility_of_element_located((By.XPATH, locator)))
+    # error_message_element = self.browser.find_element(By.XPATH, locator)
     return error_message_element.text
   
   def close_browser(self):
